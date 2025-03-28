ab41c25c7eeaf419b9a9467dcaedc2cc167cbc5c
robertderidder <c.d.ridder@student.tue.nl>
2025-03-28 12:17:41 +0100

    v

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 20 \
+    --epochs 22 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
